5737d75bed8dd81b22d175084ef5280cfe5200c6
Dr-Gunter-St <vladleonovv@gmail.com>
2017-01-22 19:27:16 +0200

    First commit

---
+++ src/Hotel/Hotel.java
@@ -0,0 +1,15 @@
+package Hotel;
+
+import Hotel.Room.Room;
+
+import java.util.List;
+
+/**
+ * Created by Влад on 22.01.2017.
+ */
+public class Hotel {
+    private long id;
+    private String name;
+    private String city;
+    private List<Room> rooms;
+}
---
+++ src/Hotel/Room/Room.java
@@ -0,0 +1,13 @@
+package Hotel.Room;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Влад on 22.01.2017.
+ */
+public class Room {
+    private long id;
+    private int price;
+    private int persons;
+    private GregorianCalendar dateAvailableFrom;
+}
